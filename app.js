a2baa6785e0849c39681843d504a8275bded7841
luizaseibelv <luizaseibelv@gmail.com>
2022-08-10 16:22:16 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
- 
+console.log("aplicação está se conectando ao banco de dados")
+// o paulo fez esse console log de teste:
+console.log("O paulo fz esse commit sem você ver na aula!")
+console.log("Enviando para o github")
+
